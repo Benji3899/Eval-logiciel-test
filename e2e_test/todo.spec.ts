b53d127c901038deb3bc1aa6d8d558e024e2f8f4
Kristen Garnier <kristen@oktopod.io>
2024-01-11 09:56:26 +0100

    Feature : install and use playwright for testing

---
+++ e2e_test/todo.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('I can create a task and it appears in the task list', async ({
+  page,
+}) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByPlaceholder('Add a new task').click();
+  await page.getByPlaceholder('Add a new task').fill('Test tache');
+  await page.getByRole('button', { name: 'Add' }).click();
+
+  const list = page.getByRole('listitem');
+
+  await expect(list).toHaveCount(1);
+  await expect(page.getByText('Test tache')).toBeDefined();
+});
+
+test('I can delete a task in task list', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByPlaceholder('Add a new task').click();
+  await page.getByPlaceholder('Add a new task').fill('Delete task');
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByRole('button').nth(2).click();
+
+  const list = page.getByRole('listitem');
+
+  await expect(list).toHaveCount(0);
+  await expect(page.getByText('Test tache')).not.toBeInViewport();
+});
